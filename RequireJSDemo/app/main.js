a8b28a2bb871d6105c359de153a736edd4dfe1e4
Dilan Arandara <dilan.madu@gmail.com>
2013-03-22 00:03:09 +0530

    Changed locations of java-script files in to meaningful folders.

--- RequireJSDemo/app/main.js
+++ RequireJSDemo/app/main.js
@@ -2,7 +2,10 @@
     requirejs.config(
         {
             paths: {
-                'jquery': '/Scripts/jquery-1.8.2.min'
+                'jquery': '/Scripts/jquery-1.8.2.min',
+                'caller': '/app/viewmodels/caller',
+                'dataservice': '/app/services/dataservice',
+                'config': '/app/configs/config'
             }
         }
     );
